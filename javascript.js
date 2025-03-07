9a6a2f115ac4a9f4fc41bbd6e56cbf26bca799ac
Sunrise-Yoake <181750089+Sunrise-Yoake@users.noreply.github.com>
2025-03-08 01:47:46 +0300

    logic of human choice

--- javascript.js
+++ javascript.js
@@ -1,16 +1,21 @@
 function getComputerChoice() {
-    const randomNumber = Math.random(); 
-    
-    if (randomNumber < 0.33) {
-      return "камень";
-    } else if (randomNumber < 0.66) {
-      return "бумага";
-    } else {
-      return "ножницы";
-    }
-  }
+  const randomNumber = Math.random(); 
   
-  console.log(getComputerChoice());
+  if (randomNumber < 0.33) {
+    return "камень";
+  } else if (randomNumber < 0.66) {
+    return "бумага";
+  } else {
+    return "ножницы";
+  }
+}
+  
+function getHumanChoice() {
+    let humanChoice= prompt("Выберите: камень, бумага или ножницы").toLowerCase();
+    return humanChoice;
+}
+
+  
   
 
 
