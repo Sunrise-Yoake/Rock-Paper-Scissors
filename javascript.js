82606f6af0c6bc1ae8cf34500f5e443407585b33
Sunrise-Yoake <181750089+Sunrise-Yoake@users.noreply.github.com>
2025-03-08 02:28:13 +0300

    logic for 1 round

--- javascript.js
+++ javascript.js
@@ -16,7 +16,41 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice,computerChoice){
-
+    switch(humanChoice) {
+        case 'камень':
+            if(computerChoice === 'камень') {
+                console.log('Ничья');
+            } else if (computerChoice === 'ножницы') {
+                console.log('Вы победили! Камень бьёт ножницы!');
+                humanScore++;
+            } else {
+                console.log('Вы проиграли! Бумага накрывает камень!');
+                computerScore++;
+            }
+            break;
+        case 'бумага':
+            if(computerChoice  === 'бумага') {
+                console.log('Ничья');
+            } else if (computerChoice === 'камень') {
+                console.log('Вы победили! Бумага накрывает камень!');
+                humanScore++;
+            } else {
+                console.log('Вы проиграли! Ножницы режут бумагу!');
+                computerScore++;
+            }
+            break;
+        case 'ножницы':
+            if(computerChoice === 'ножницы') {
+                console.log('Ничья');
+            } else if (computerChoice === 'бумага') {
+                console.log('Вы победили! Ножницы режут бумагу!');
+                humanScore++;
+            } else {
+                console.log('Вы проиграли! Камень бьёт ножницы!');
+                computerScore++;
+            }
+        break;
+    }   
 }
 
   
