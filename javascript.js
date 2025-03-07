47a3b69af333856a63d95317b6bf1be546da4892
Sunrise-Yoake <181750089+Sunrise-Yoake@users.noreply.github.com>
2025-03-07 20:31:54 +0300

    computer selection logic

--- javascript.js
+++ javascript.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+function getComputerChoice() {
+    const randomNumber = Math.random(); 
+    
+    if (randomNumber < 0.33) {
+      return "камень";
+    } else if (randomNumber < 0.66) {
+      return "бумага";
+    } else {
+      return "ножницы";
+    }
+  }
+  
+  console.log(getComputerChoice());
+  
+
+
+
+ 
+  
